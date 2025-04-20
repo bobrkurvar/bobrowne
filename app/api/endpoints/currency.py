01f5de801835dbf590083cacb73a2d3dbc3245e7
Andy <adnrey.bogdanov2005@mail.ru>
2025-04-20 10:06:06 +0300

    добваил класс для обрашени ко внешнему api

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -1,23 +1,13 @@
-from sys import prefix
 
 from fastapi import APIRouter, HTTPException, status
-from pathlib import Path
-from app.core.config import load_config
 from app.core.security import getUserFromToken
 from app.db.session import getConnectDep
 from app.db.models.user import User
-from app.utils import external_api
+
 
 router = APIRouter()
 
-router.include_router(
-    external_api.router,
-    prefix="/currency"
-)
 
-path = Path(r'C:\project1\.env')
-conf = load_config(path)
-api_key = conf.CURRENCY_API_KEY
 
 @router.get('/exchange')
 async def exchange(username: getUserFromToken, session: getConnectDep):
--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -1,5 +1,41 @@
 import aiohttp
+from app.core.config import load_config
+from pathlib import Path
+import asyncio
+from app.utils.schemas import Currency
+
+path = Path(r'C:\project1\.env')
+conf = load_config(path)
+api_key = str(conf.CURRENCY_API_KEY)
 
 class ExternalAPI:
     def __init__(self):
-        self.client = aiohttp.ClientSession
+        self.session = aiohttp.ClientSession()
+        self.url = 'https://api.apilayer.com/currency_data/'
+        self._cur_list = None
+
+    @property
+    async def cur_list(self):
+        if not self._cur_list:
+            async with self.session.get(self.url+'list', headers={'apikey': api_key}) as resp:
+                self._cur_list = await resp.json()
+        return self._cur_list
+
+
+    async def convert(self, amount: float = 1, to: str = 'RUB', _from: str = 'USD'):
+        async with self.session.get(self.url+'convert', headers={'apikey': api_key}, params={"from": _from, "to": to, "amount": amount}) as resp:
+            return await resp.json()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.close()
+
+async def main():
+    async with ExternalAPI() as ex:
+        r = await ex.cur_list
+        print(r)
+
+asyncio.run(main())
+
